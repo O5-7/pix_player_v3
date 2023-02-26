f1a0c3008249b09c76460dbe51f73542ebf5e6e6
517205163 <517205163@qq.com>
2023-02-26 12:52:11 +0800

    meta_data reading font size and type screen size

--- main.cpp
+++ main.cpp
@@ -2,9 +2,53 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <chrono>
 
 
 using namespace std;
+
+/**
+ * @brief string转 wchar_t *
+ * @param pKey string
+ * @return wchar_t*
+ * */
+wchar_t *MultiByte_To_WideChar(const string &pKey) {
+    const char *pCStrKey = pKey.c_str();
+    //第一次调用返回转换后的字符串长度，用于确认为wchar_t*开辟多大的内存空间
+    int pSize = MultiByteToWideChar(CP_OEMCP, 0, pCStrKey, strlen(pCStrKey) + 1, NULL, 0);
+    wchar_t *pWCStrKey = new wchar_t[pSize];
+    //第二次调用将单字节字符串转换成双字节字符串
+    MultiByteToWideChar(CP_OEMCP, 0, pCStrKey, strlen(pCStrKey) + 1, pWCStrKey, pSize);
+    return pWCStrKey;
+}
+
+/**
+ * @brief 显示/隐藏 光标
+ * @param visible 是否可见
+ * @return 返回说明
+ *     -<em>false</em> 失败
+ *     -<em>true</em> 成功
+ * */
+bool Show_Cursor(bool visible) { //显示或隐藏光标
+    CONSOLE_CURSOR_INFO cursor_info = {100, visible};
+    return SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
+}
+
+
+/**
+ * @brief 设置窗口位置
+ * @param screen_x
+ * @param screen_y
+ *
+ * @return 返回说明
+ *     -<em>false</em> 失败
+ *     -<em>true</em> 成功
+ */
+bool Set_Windows_Console_Pos(short x, short y) {
+    HANDLE hConsole_out = GetStdHandle(STD_OUTPUT_HANDLE);
+    bool success = SetConsoleCursorPosition(hConsole_out, {x, y});
+    return success;
+}
 
 /**
  * @brief 设置窗口大小
@@ -21,6 +65,8 @@ bool Set_Windows_size_without_scrollbar(short screen_x, short screen_y) {
     HANDLE hConsole_out = GetStdHandle(STD_OUTPUT_HANDLE);
     bool success2 = SetConsoleScreenBufferSize(hConsole_out, {screen_x, screen_y});
     bool success1 = SetConsoleWindowInfo(hConsole_out, 1, &rc);
+    success2 = SetConsoleScreenBufferSize(hConsole_out, {screen_x, screen_y});
+    success1 = SetConsoleWindowInfo(hConsole_out, 1, &rc);
     if (success1 & success2) return 1;
     else {
         cout << "Set_window_size" << success1 << endl;
@@ -39,7 +85,7 @@ bool Set_Windows_size_without_scrollbar(short screen_x, short screen_y) {
  *     -<em>false</em> 失败
  *     -<em>true</em> 成功
  */
-bool Set_Windows_font(short size_x, short size_y) {
+bool Set_Windows_font(short size_x, short size_y, const wchar_t *font_type) {
     CONSOLE_FONT_INFOEX cfi;
     cfi.cbSize = sizeof(CONSOLE_FONT_INFOEX);
     cfi.nFont = 0;
@@ -47,7 +93,7 @@ bool Set_Windows_font(short size_x, short size_y) {
     cfi.dwFontSize.Y = size_y;
     cfi.FontFamily = FF_DONTCARE;
     cfi.FontWeight = FW_DONTCARE;
-    wcscpy_s(cfi.FaceName, L"新宋体"); // 更改字体名
+    wcscpy_s(cfi.FaceName, font_type); // 更改字体名
     return SetCurrentConsoleFontEx(GetStdHandle(STD_OUTPUT_HANDLE), FALSE, &cfi);
 }
 
@@ -78,55 +124,91 @@ bool EnableVTMode() {
 }
 
 int main(int argc, char *argv[]) {
-
-    Set_Windows_font(8, 16);
-    SetConsoleTitle("pix_player_v3");
-    SetWindowPos(GetConsoleWindow(), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+//   初始设置
     EnableVTMode();
-    //cout << "\033[38;2;255;255;255m"; // << "\033[0m";
+    Show_Cursor(false);
+    SetWindowPos(GetConsoleWindow(), HWND_TOPMOST, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
 
+//  读取元数据
     ifstream codes_result;
     string codes;
-    codes_result.open("C:\\Users\\Normal\\PycharmProjects\\zi_fu_hua_v3\\code_sources_v3.txt");
-    getline(codes_result, codes);
-    codes_result.close();
-    bool wchar = false;
-    bool color = true;
-    for (char i: codes) {
-        wchar = !wchar;
-        if (wchar) {
-            color = !color;
-            if (color) cout << "\033[38;2;255;255;255;48;2;12;12;12m";
-            else cout << "\033[48;2;255;255;255;38;2;12;12;12m";
+
+//  元数据
+    string font_type;
+    int font_size;
+    int str_size_X;
+    int str_size_Y;
+    bool loop_play = false;
+    bool audio = false;
+
+    codes_result.open(R"(C:\Users\Normal\PycharmProjects\zi_fu_hua_v3\aya\result.zfh)");
+//  读取元数据 直到END_META_DATA
+    while (getline(codes_result, codes)) {
+        if (codes.substr(0, 2) == "//") continue; // 忽略 //
+        if (codes == "END_META_DATA") {
+            Set_Windows_font(
+                    short(font_size / 2),
+                    short(font_size),
+                    MultiByte_To_WideChar(font_type)
+            );
+            Set_Windows_size_without_scrollbar(short(str_size_X * 2 + 1), short(str_size_Y + 1));
+            // 禁止修改窗口大小
+            SetWindowLongPtrA(GetConsoleWindow(), GWL_STYLE, GetWindowLongPtrA(GetConsoleWindow(), GWL_STYLE) & ~WS_SIZEBOX & ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX);
+            break;
         }
-        cout << i;
-    }
-    codes_result.open("C:\\Users\\Normal\\PycharmProjects\\zi_fu_hua_v3\\code_sources_v3.txt");
-    getline(codes_result, codes);
-    codes_result.close();
-    codes_result.close();
-    wchar = false;
-    color = false;
-    for (char i: codes) {
-        wchar = !wchar;
-        if (wchar) {
-            color = !color;
-            if (color) cout << "\033[38;2;255;255;255;48;2;12;12;12m";
-            else cout << "\033[48;2;255;255;255;38;2;12;12;12m";
+        int index = int(codes.find('='));
+        if (codes.substr(0, index) == "media_name") {
+//          media_name
+            string file_name = codes.substr(index + 1, codes.length() - index - 1);
+            SetConsoleTitle(("pix_player_v3: " + file_name).data());
         }
-        cout << i;
+        if (codes.substr(0, index) == "font_type") {
+//          字体名字
+            font_type = codes.substr(index + 1, codes.length() - index - 1);
+        }
+        if (codes.substr(0, index) == "font_size") {
+//           字体大小
+            font_size = stoi(codes.substr(index + 1, codes.length() - index - 1));
+        }
+        if (codes.substr(0, index) == "str_size") {
+//           屏幕大小
+            string _str_size = codes.substr(index + 1, codes.length() - index - 1);
+            int tuple_index = int(_str_size.find(','));
+            str_size_X = stoi(_str_size.substr(0, tuple_index));
+            str_size_Y = short(stoi(_str_size.substr(tuple_index + 1, _str_size.length() - tuple_index - 1)));
+        }
+        if (codes.substr(0, index) == "loop_play") {
+//           是否循环播放
+            string _loop_play = codes.substr(index + 1, codes.length() - index - 1);
+            loop_play = _loop_play == "True";
+        }
+        if (codes.substr(0, index) == "audio") {
+//           是否播放音频
+            string _audio = codes.substr(index + 1, codes.length() - index - 1);
+            loop_play = _audio == "True";
+        }
     }
-    cout << "\033[0m";
-    codes_result.open("color.txt");
-    getline(codes_result, codes);
+// TODO:
+//  audio read and play
+//  playing duration
+//  loop play
+
+//    cout << "\033[38;2;255;255;255m"; // << "\033[0m";
+    int line = 0;
+    while (getline(codes_result, codes)) {
+        if (codes.substr(0, 2) == "//") continue;
+        if (codes.substr(0, 9) == "duration=") {
+            float duration = stof(codes.substr(9, codes.length() - 9));
+            continue;
+        }
+        cout << codes << endl;
+        line++;
+        if (line % str_size_Y == 0) {
+            Set_Windows_Console_Pos(0, 0);
+        }
+    }
     codes_result.close();
-    cout << codes;
-// 禁止修改窗口大小
-    SetWindowLongPtrA(
-            GetConsoleWindow(),
-            GWL_STYLE,
-            GetWindowLongPtrA(GetConsoleWindow(), GWL_STYLE)
-            & ~WS_SIZEBOX & ~WS_MAXIMIZEBOX & ~WS_MINIMIZEBOX
-    );
+
+    cout << endl;
     getchar();
 }
